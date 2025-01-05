1e3743a3af18cecae9e4306bf6ce4ad8d27cc3d1
Accoder777 <rozimurodjumanazarovv@gmail.com>
2025-01-05 15:14:42 +0500

    done home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import BodyTop from "../../components/bodyTop/BodyTop";
 import styles from "./home.module.css";
 import FilmsDisplay from "../../components/filmDisplay/FilmsDisplay";
@@ -6,6 +6,7 @@ import Tabs from "../../components/ui/Tabs/Tabs";
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import { useDebounce } from "use-debounce";
+import { useGetTrendingMovieQuery } from "../../features/movie/movieApi";
 
 const list = [
   {
@@ -23,56 +24,22 @@ const list = [
 ];
 
 const Home = () => {
-  
-  const [Inputvalue, setInputValue] = useState("");
-  const [isLoading, setIsloading] = useState(false);
-  const [connectionErr, setConnnectionErr] = useState(false);
-  const [moviesData, setMoviesData] = useState([]);
+  // states
+  const [search, setSearch] = useState("");
   const [activeTab, setActiveTab] = useState("all");
-  const [debounceFilter] = useDebounce(Inputvalue, 1000)
+  const [debounceValue] = useDebounce(search, 1000)
 
-  // useEffect(() => {
-  //     const getData = async () => {
-  //         setIsloading(true);
-  //         try {
-  //             const tempData = await getTrending(activeTab);
-  //             setMoviesData(tempData.data.results);
+  // API
+  const trendingRes = useGetTrendingMovieQuery(activeTab);
+  console.log('trendingRes', trendingRes)
 
-  //     } catch (error) {
-  //       HandleApiRespErr(error)
-  //       setConnnectionErr(true)
-        
-  //     } finally {
-  //       setIsloading(false);
-  //     }
-  //   };
-  //   getData();
-  // }, [activeTab]);
-
-
-  // const handleFindData = async (Inputvalue)=>{
-  //   setInputValue(Inputvalue  )
-  //   setIsloading(true)
-  //   try {
-  //     const res = await findAll(Inputvalue)
-  //     if(res.data.results){
-  //       const tempData = res.data.results.filter((item)=> item.media_type!=='person')
-  //       setMoviesData(tempData)
-
-  //     }
-      
-  //   } catch (error) {
-  //     HandleApiRespErr(error)
-  //   }finally{
-  //     setIsloading(false)
-  //   }
-  // }
-  // useEffect(()=>{
-  //   if(debounceFilter){
-  //     handleFindData(debounceFilter)
-  //   } 
-    
-  // },[debounceFilter])
+  // Memo 
+  const trendingMovieData = useMemo(()=>{
+    if(!trendingRes.isLoading && trendingRes.data && trendingRes.data?.results){
+        return trendingRes.data?.results
+    }
+    return []
+  },[trendingRes.isLoading, trendingRes.data])
 
   const handleChangeTab = ({ key }) => {
     setActiveTab(key);
@@ -85,16 +52,16 @@ const Home = () => {
         BodyTopDesc={
           "List of movies and TV Shows, I, Pramod Poudel have watched till date.Explore what I have watched and also feel free to make a suggestion. 😉"
         }
-        value={Inputvalue}
-        setValue={setInputValue}
+        value={search}
+        setValue={setSearch}
       />
       <Tabs list={list} onClick={handleChangeTab} activeItem={activeTab} />
       <div className={styles.container}>
         <div className={styles.internalNav}></div>
         <h1 className={styles.filmCounter}>
-          All <span>({moviesData?.length})</span>
+          All <span>({trendingMovieData?.length})</span>
         </h1>
-        {isLoading ? (
+        {trendingRes.isLoading || trendingRes.isFetching? (
           <div style={{marginLeft: '20px'}}>
             <Skeleton 
               height={480} 
@@ -110,10 +77,8 @@ const Home = () => {
               marginLeft={'1rem'}
             />
           </div>
-        ) : connectionErr ? (
-          <div>Connection error occurred</div>
-        ) : (    
-            moviesData?.map((item) => (
+        )  : (    
+            trendingMovieData?.map((item) => (
               <FilmsDisplay {...item} key={item.id} />
             ))         
         )}
